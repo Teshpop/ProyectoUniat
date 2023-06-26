2af72805c7d2a3bf31f37c32b926bd8c1d768024
ArturoEscobar-dev <veloctur@gmail.com>
2023-06-26 14:51:50 -0600

    Cooldown de cuchillo
    Hice cambios en el script de Dispara Jugdor para que el jugador solo pueda disparar despues de un cooldown establecido en una variable pública, lo dejé en 2seg, pero se puede poner lo segundos que quieran.

--- ProyectoUniat/Assets/Jugador/Scripts/DisparoJugador.cs
+++ ProyectoUniat/Assets/Jugador/Scripts/DisparoJugador.cs
@@ -7,18 +7,28 @@ public class DisparoJugador : MonoBehaviour
     public Transform fireP;
     public GameObject knifepref;
     public float time2Liv;
-    // Update is called once per frame
+    public float cooldownTime = 5f;
+    private bool canShoot = true;
+
     void Update()
     {
-        if (Input.GetButtonDown("Fire2"))
+        if (Input.GetButtonDown("Fire2") && canShoot)
         {
             Shoot();
+            StartCoroutine(Cooldown());
         }
     }
 
     void Shoot()
     {
-        GameObject KAct = Instantiate(knifepref, fireP.position,fireP.rotation);
-        Destroy (KAct,time2Liv);
+        GameObject KAct = Instantiate(knifepref, fireP.position, fireP.rotation);
+        Destroy(KAct, time2Liv);
+    }
+
+    IEnumerator Cooldown()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(cooldownTime);
+        canShoot = true;
     }
 }
